a2ce20ebc9d2866a1f27685174ed360937c00b33
Govo-Mein <72652344+Govo-Mein@users.noreply.github.com>
2024-11-01 12:55:21 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,22 +1,32 @@
 let score = 0;
+let openMouth = false; // для відстеження відкритого рота
+let fishCount = 0; // для підрахунку рибок
 
 function changeCat() {
     const cat = document.getElementById('cat');
-    const food = document.getElementById('food');
-    const scoreDisplay = document.getElementById('score');
     const originalSrc = 'https://github.com/Govo-Mein/picture/blob/main/image__2_-removebg-preview.png?raw=true';
     const newSrc = 'https://github.com/Govo-Mein/picture/blob/main/image__3_-removebg-preview%20(1).png?raw=true';
 
-    cat.src = newSrc;
+    if (!openMouth) {
+        cat.src = newSrc;
+        openMouth = true;
+    }
 
-    setTimeout(() => {
-        cat.src = originalSrc;
-    }, 2000); // 2000 мс = 2 секунди
+    dropFood();
+}
 
-    // Зробити рибку видимою і встановити її початкову позицію
-    food.style.display = 'block';
+function dropFood() {
+    const food = document.createElement('img');
+    const scoreDisplay = document.getElementById('score');
+    const cat = document.getElementById('cat');
+    fishCount++; // Збільшуємо кількість рибок
+
+    // Створюємо нову рибку
+    food.src = 'https://github.com/Govo-Mein/picture/blob/main/fish-removebg-preview.png?raw=true';
+    food.className = 'food';
     food.style.top = '170px'; // Початкове положення нижче котика
-    food.style.left = 'calc(50% - 18px)'; // Зсунемо рибку на 5 пікселів вправо
+    food.style.left = 'calc(50% - 18px)'; // Зсунемо рибку на 3 пікселі вліво
+    document.body.appendChild(food);
 
     // Дати їй плавне падіння
     setTimeout(() => {
@@ -26,7 +36,15 @@ function changeCat() {
     // Приховати рибку після досягнення кінцевого положення
     setTimeout(() => {
         food.style.display = 'none';
+        document.body.removeChild(food);
         score++;
         scoreDisplay.textContent = `Кількість натискань: ${score}`;
-    }, 2000); // 2 секунди для плавного падіння + 100 мс затримки
+
+        fishCount--; // Зменшуємо кількість рибок
+        // Перевірка, чи всі рибки зникли
+        if (fishCount === 0) {
+            cat.src = 'https://github.com/Govo-Mein/picture/blob/main/image__2_-removebg-preview.png?raw=true';
+            openMouth = false;
+        }
+    }, 1600); // 1.6 секунди для плавного падіння + 100 мс затримки
 }
